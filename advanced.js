03c7b579938131af591519fefae62af2407d3922
Jeremy Winter <jeremywinter@users.noreply.github.com>
2024-08-19 15:46:01 -0500

    updated advanced

--- advanced.js
+++ advanced.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d'); // 2d or webGl
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const particlesArray = [];
+let hue = 0;
 
 window.addEventListener('resize', function(){
   canvas.width = window.innerWidth;
@@ -17,52 +18,61 @@ const mouse = {
 canvas.addEventListener('click', function(event){
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 10; i++ ) {
+    particlesArray.push(new Particle());
+  }
 });
 
 canvas.addEventListener('mousemove', function(event){
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 5; i++ ) {
+    particlesArray.push(new Particle());
+  }
 });
 
 class Particle {
   constructor(){
-    //this.x = mouse.x;
-    //this.y = mouse.y;
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 5 + 1;
+    this.x = mouse.x;
+    this.y = mouse.y;
+    // this.x = Math.random() * canvas.width;
+    // this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 16 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
+    this.color = 'hsl(' + hue + ', 100%, 50%)';
   }
   update(){
     this.x += this.speedX;
     this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1; // shrink as they move
   }
   draw(){
-    ctx.fillStyle = 'limegreen';
+    ctx.fillStyle = this.color;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 20, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill(); //call the fill method
   }
 }
-
-function createParticles(){
-  for (let i = 0; i < 100; i++){
-    particlesArray.push(new Particle());
-  }
-}
-createParticles();
 
 function handleParticles(){
   for (let i = 0; i < particlesArray.length; i++){
     particlesArray[i].update();
     particlesArray[i].draw();
+    if (particlesArray[i].size <= 0.3){ 
+      particlesArray.splice(i, 1); // particals that shrink are then removed from Array
+      console.log(particlesArray.length); 
+      i--;
+    } 
   }
 }
 
 function animate(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = 'rgba(0,0,0,0.02)';
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
+  hue+=2;
   requestAnimationFrame(animate);
 }
 animate();
